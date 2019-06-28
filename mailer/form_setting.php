c94d44cae2b0c3c427c49bb540a71b8ea6fe5029
salonicodes <45900324+salonicodes@users.noreply.github.com>
2019-06-28 20:41:01 +0530

    Added FInal Files

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from Saloni Trioathi Resume'"; 
+	$to = 'engineersaloni@gmail.com';
+	$from = 'engineersaloni@gmail.com';
+	$fromName = 'Saloni Tripathi';
+	$charset = 'UTF-8';
+?>
